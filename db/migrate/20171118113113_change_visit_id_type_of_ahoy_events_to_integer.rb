3d4e1d05004cf4a25c9e3f1654fb652264e97900
James Mason <jmason@suse.com>
2017-12-18 14:08:04 -0800

    FIXUP visit id migration

--- db/migrate/20171118113113_change_visit_id_type_of_ahoy_events_to_integer.rb
+++ db/migrate/20171118113113_change_visit_id_type_of_ahoy_events_to_integer.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
 
 class ChangeVisitIdTypeOfAhoyEventsToInteger < ActiveRecord::Migration[5.0]
-  def change
-    change_column :ahoy_events, :visit_id, :integer, limit: nil
+  def up
+    drop_table "ahoy_events"
+    create_table "ahoy_events", force: :cascade do |t|
+      t.integer  "visit_id"
+      t.integer  "user_id"
+      t.string   "name"
+      t.text     "properties"
+      t.datetime "time"
+      t.index ["time"], name: "index_ahoy_events_on_time"
+      t.index ["user_id"], name: "index_ahoy_events_on_user_id"
+      t.index ["visit_id"], name: "index_ahoy_events_on_visit_id"
+    end
   end
+
+  def down
+    drop_table "ahoy_events"
+    create_table "ahoy_events", force: :cascade do |t|
+      t.uuid  "visit_id"
+      t.integer  "user_id"
+      t.string   "name"
+      t.text     "properties"
+      t.datetime "time"
+      t.index ["time"], name: "index_ahoy_events_on_time"
+      t.index ["user_id"], name: "index_ahoy_events_on_user_id"
+      t.index ["visit_id"], name: "index_ahoy_events_on_visit_id"
+    end
+  end
+
 end
